1d017c23f5735f61e691c6a6274be9d32f0f327b
Seiya Nuta <nuta@seiya.me>
2020-05-23 23:54:33 +0900

    Update

--- tools/analyzer/components/log_stream.tsx
+++ tools/analyzer/components/log_stream.tsx
@@ -3,46 +3,18 @@ import * as randomColor from "randomcolor";
 export default function LogStream({ items }) {
     return (
         <div className="log-stream">
-            <table className="items">
-                <thead>
-                    <tr>
-                        <th>Timestamp</th>
-                        <th>Task</th>
-                        <th>Level</th>
-                        <th>Message</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.map(e => (
-                        <tr key={e.timestamp} className="item">
-                            <td style={{ width: "15%" }}>{e.timestamp}</td>
-                            <td style={{ width: "10%" }}>
-                                <span style={{
-                                    color: "white",
-                                    fontWeight: "bold",
-                                    fontSize: "0.78rem",
-                                    borderRadius: "3px",
-                                    padding: "1px 5px",
-                                    textAlign: "center",
-                                    background: randomColor({
-                                        luminosity: 'dark',
-                                        seed: e.task
-                                    })
-                                }}>{e.task}</span>
-                            </td>
-                            <td style={{ width: "10%" }}>{e.level}</td>
-                            <td>{e.message}</td>
-                        </tr>
-                    ))}
-                    <tr className="anchor">
-                        <td colSpan="4">
-                            <span className="anchor-message">
-                                bottom of log
-                            </span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            {items.map(e => (
+                <p key={e.timestamp} className="item">
+                    <span className="timestamp">{e.timestamp}</span>
+                    <span className="task" style={{
+                        background: randomColor({
+                            luminosity: 'dark',
+                            seed: e.task
+                        })
+                    }}>{e.task}</span>
+                    <span className="message">{e.message}</span>
+                </p>
+            ))}
             <style jsx>{`
                 .log-stream {
                     max-height: 200px;
@@ -52,38 +24,33 @@ export default function LogStream({ items }) {
                     font-size: 0.9rem;
                 }
 
-                .items {
-                    border-collapse: collapse;
-                    width: 100%;
-                }
-
                 .item {
-                    overflow-anchor: none;
+                    margin: 5px 0;
                 }
 
-                thead th {
-                    position: sticky;
-                    top: 0;
-                    z-index: 1;
-                    background: #efefef;
+                .timestamp {
+                    display: inline-block;
+                    width: 80px;
                 }
 
-                tbody td {
-                    padding: 4px 0px;
-                }
-
-                .anchor {
-                    overflow-anchor: auto;
-                    height: 1px;
+                .task {
+                    display: inline-block;
+                    width: 80px;
+                    color: white;
+                    font-weight: bold;
+                    font-size: 0.78rem;
+                    border-radius: 3px;
+                    padding: 1px 5px;
                     text-align: center;
                 }
 
-                .anchor-message {
-                    border: 1px solid #7a7a7a;
-                    background: #efefef;
-                    padding: 2px 20px;
-                    border-radius: 5px;
-                    font-weight: bold;
+                .level {
+                    display: inline-block;
+                }
+
+                .message {
+                    display: inline-block;
+                    margin-left: 1rem;
                 }
             `}</style>
         </div>
