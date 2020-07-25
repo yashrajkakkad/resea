61eecd386dd92558b5fc45282651d3c5c6303449
Seiya Nuta <nuta@seiya.me>
2020-07-25 02:34:05 +0000

    Update

--- tools/analyzer/components/log_stream.tsx
+++ tools/analyzer/components/log_stream.tsx
@@ -15,47 +15,6 @@ export default function LogStream({ items }) {
                     <span className="message">{e.message}</span>
                 </p>
             ))}
-            <style jsx>{`
-                .log-stream {
-                    height: 100%;
-                    background: #f2f2f2;
-                    padding: 5px 10px;
-                    box-sizing: border-box;
-                    width: 100%;
-                    overflow: scroll;
-                    font-family: var(--font-code);
-                    font-size: 0.9rem;
-                }
-
-                .item {
-                    margin: 5px 0;
-                }
-
-                .timestamp {
-                    display: inline-block;
-                    width: 80px;
-                }
-
-                .task {
-                    display: inline-block;
-                    width: 80px;
-                    color: white;
-                    font-weight: bold;
-                    font-size: 0.78rem;
-                    border-radius: 3px;
-                    padding: 1px 5px;
-                    text-align: center;
-                }
-
-                .level {
-                    display: inline-block;
-                }
-
-                .message {
-                    display: inline-block;
-                    margin-left: 1rem;
-                }
-            `}</style>
         </div>
     )
 }
--- tools/analyzer/pages/index.tsx
+++ tools/analyzer/pages/index.tsx
@@ -2,7 +2,10 @@ import io from 'socket.io-client';
 import { useState, useEffect, useRef } from 'react';
 import LogStream from "../components/log_stream";
 import TimeSeriesGraph from "../components/time_series_graph";
-import { Grid, Container, Paper, makeStyles, Typography } from '@material-ui/core';
+import {
+    Grid, Container, Paper, makeStyles, Typography, Box,
+    Table, TableContainer, TableHead, TableBody, TableRow, TableCell
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     welcomePaper: {
@@ -11,13 +14,29 @@ const useStyles = makeStyles((theme) => ({
       marginBottom: theme.spacing(1),
     },
     graphPaper: {
-        padding: theme.spacing(2),
+        padding: theme.spacing(1),
         display: 'flex',
         overflow: 'auto',
         flexDirection: 'column',
     },
     graph: {
         height: 200,
+    },
+    logPaper: {
+        marginTop: theme.spacing(2),
+        padding: theme.spacing(2),
+    },
+    logStream: {
+        marginTop: theme.spacing(1),
+        height: 200,
+        overflow: "scroll",
+    },
+    tasksPaper: {
+        marginTop: theme.spacing(2),
+        padding: theme.spacing(2),
+    },
+    tasksTable: {
+        marginTop: theme.spacing(2),
     }
 }));
 
@@ -50,17 +69,17 @@ export default function Home() {
             switch (e.key) {
                 case "kernel.cpu_load":
                     setCpuLoadData(prev =>
-                        [...prev.slice(-10), { x: e.timestamp, y: e.value }]
+                        [...prev.slice(-20), { x: e.timestamp, y: e.value }]
                     );
                 break;
                 case "kernel.mem_used":
                     setKernelMemUsedData(prev =>
-                        [...prev.slice(-10), { x: e.timestamp, y: e.value }]
+                        [...prev.slice(-20), { x: e.timestamp, y: e.value }]
                     );
                 break;
                 case "bootstrap.mem_used":
                     setUserMemUsedData(prev =>
-                        [...prev.slice(-10), { x: e.timestamp, y: e.value }]
+                        [...prev.slice(-20), { x: e.timestamp, y: e.value }]
                     );
                 break;
             }
@@ -78,7 +97,7 @@ export default function Home() {
             <Grid container spacing={1}>
                 <Grid item xs={12} md={4} lg={3}>
                     <Paper className={classes.graphPaper}>
-                        <Typography component="h2" variant="h6" gutterBottom>
+                        <Typography component="h2" variant="h6">
                             CPU load
                         </Typography>
                         <div className={classes.graph}>
@@ -92,7 +111,7 @@ export default function Home() {
                 </Grid>
                 <Grid item xs={12} md={4} lg={3}>
                     <Paper className={classes.graphPaper}>
-                        <Typography component="h2" variant="h6" gutterBottom>
+                        <Typography component="h2" variant="h6">
                             Memory usage
                         </Typography>
                             <div className={classes.graph}>
@@ -106,7 +125,7 @@ export default function Home() {
                 </Grid>
                 <Grid item xs={12} md={4} lg={3}>
                     <Paper className={classes.graphPaper}>
-                        <Typography component="h2" variant="h6" gutterBottom>
+                        <Typography component="h2" variant="h6">
                             IPC
                         </Typography>
                         <div className={classes.graph}>
@@ -119,54 +138,51 @@ export default function Home() {
                     </Paper>
                 </Grid>
             </Grid>
-        </Container>
-    )
 
-    return (
-        <div>
-            <div>
-                <div>
-                    <div style={{ height: "200px", display: "flex" }}>
-                    </div>
-                    <div title="Log" style={{ height: "300px" }}>
-                        <div style={{ height: "200px" }}>
-                            <LogStream items={logItems}></LogStream>
-                        </div>
-                    </div>
-                </div>
-                <div style={{ width: "350px", minHeight: "500px", marginLeft: "20px" }}>
-                    <div title="Tasks" style={{ minHeight: "500px" }}>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Task ID</th>
-                                    <th>Name</th>
-                                    <th>State</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {tasks.map(task => (
-                                    <tr key={task.id}>
-                                        <td>{task.id}</td>
-                                        <td>{task.name}</td>
-                                        <td>
-                                            {task.state}
-                                            {task.state == "sending"
-                                             && ` (to #${task.src_or_dst})`}
-                                            {task.state == "receiving"
-                                             && task.src
-                                             && ` (from #${task.src_or_dst})`}
-                                            {task.state == "receiving"
-                                             && !task.src
-                                             && ` (from any)`}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+            <Paper className={classes.logPaper}>
+                <Typography component="h2" variant="h6">
+                    Log
+                </Typography>
+                <Box className={classes.logStream}>
+                    <LogStream items={logItems}></LogStream>
+                </Box>
+            </Paper>
+
+            <Paper className={classes.tasksPaper}>
+                <Typography component="h2" variant="h6">
+                    Tasks
+                </Typography>
+                <TableContainer className={classes.tasksTable}>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Task ID</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>State</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {tasks.map(task => (
+                                <TableRow key={task.id}>
+                                    <TableCell>{task.id}</TableCell>
+                                    <TableCell>{task.name}</TableCell>
+                                    <TableCell>
+                                        {task.state}
+                                        {task.state == "sending"
+                                         && ` (to #${task.src_or_dst})`}
+                                        {task.state == "receiving"
+                                         && task.src
+                                         && ` (from #${task.src_or_dst})`}
+                                        {task.state == "receiving"
+                                         && !task.src
+                                         && ` (from any)`}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </Container>
     )
 }
