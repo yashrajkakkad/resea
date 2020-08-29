e6a3351c620de6a499806839f8d8b08a601a8c22
Seiya Nuta <nuta@seiya.me>
2020-08-29 15:25:59 +0900

    Update

--- tools/baremetal-ci/server.py
+++ tools/baremetal-ci/server.py
@@ -10,10 +10,15 @@ MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "baremental-ci")
 app = FastAPI()
 db = MongoClient(MONGO_URI)[MONGO_DB_NAME]
 
-@app.get('/api/hello')
+@app.get("/api/hello")
 def home():
-    return {'hello': str(db.posts.insert_one({ "asd": 123 }).inserted_id) }
+    return {"hello": str(db.posts.insert_one({ "asd": 123 }).inserted_id) }
 
-if __name__ == '__main__':
+@app.get("/api/runs")
+def runs_list():
+    for x in db.posts.find():
+      print(x)
+
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run("server:app", reload=True)
