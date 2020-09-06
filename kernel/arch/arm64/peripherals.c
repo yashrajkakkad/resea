3171e22027ebc288846d1006f962a5c5d68ce63c
Seiya Nuta <nuta@seiya.me>
2020-09-06 21:20:22 +0900

    Update

--- kernel/arch/arm64/peripherals.c
+++ kernel/arch/arm64/peripherals.c
@@ -13,6 +13,107 @@ char uart_getc() {
     return mmio_read(UART0_DR);
 }
 
+
+
+/* a properly aligned buffer */
+extern volatile unsigned int mbox[36];
+
+#define MMIO_BASE       0x3F000000 + 0xffff000000000000
+#define MBOX_REQUEST    0
+
+/* channels */
+#define MBOX_CH_POWER   0
+#define MBOX_CH_FB      1
+#define MBOX_CH_VUART   2
+#define MBOX_CH_VCHIQ   3
+#define MBOX_CH_LEDS    4
+#define MBOX_CH_BTNS    5
+#define MBOX_CH_TOUCH   6
+#define MBOX_CH_COUNT   7
+#define MBOX_CH_PROP    8
+
+/* tags */
+#define MBOX_TAG_GETSERIAL      0x10004
+#define MBOX_TAG_SETCLKRATE     0x38002
+#define MBOX_TAG_LAST           0
+/* mailbox message buffer */
+volatile unsigned int  __attribute__((aligned(16))) mbox[36];
+
+#define VIDEOCORE_MBOX  (MMIO_BASE+0x0000B880)
+#define MBOX_READ       ((volatile unsigned int*)(VIDEOCORE_MBOX+0x0))
+#define MBOX_POLL       ((volatile unsigned int*)(VIDEOCORE_MBOX+0x10))
+#define MBOX_SENDER     ((volatile unsigned int*)(VIDEOCORE_MBOX+0x14))
+#define MBOX_STATUS     ((volatile unsigned int*)(VIDEOCORE_MBOX+0x18))
+#define MBOX_CONFIG     ((volatile unsigned int*)(VIDEOCORE_MBOX+0x1C))
+#define MBOX_WRITE      ((volatile unsigned int*)(VIDEOCORE_MBOX+0x20))
+#define MBOX_RESPONSE   0x80000000
+#define MBOX_FULL       0x80000000
+#define MBOX_EMPTY      0x40000000
+
+static inline void delay(unsigned clocks) {
+    while (clocks-- > 0) {
+        __asm__ __volatile__("nop");
+    }
+}
+
+/// Performs a mailbox call.
+void mbox_call(uint8_t ch, const uint8_t *mbox) {
+    ASSERT(IS_ALIGNED((vaddr_t) mbox, 0x10));
+
+    while (*MBOX_STATUS & MBOX_FULL) {
+        delay(1);
+    }
+
+    unsigned int value =
+        (((vaddr_t) mbox) & ~0xf) // The address of the mailbox to be sent.
+        | (ch & 0xf)              // Channel.
+        ;
+    *MBOX_WRITE = value;
+    while (1) {
+        while (*MBOX_STATUS & MBOX_EMPTY) {
+            delay(1);
+        }
+
+        if (value == *MBOX_READ) {
+            break;
+        }
+    }
+}
+
+void uart_init(void) {
+    // Set up the clock.
+    mbox[0] = 9*4;
+    mbox[1] = MBOX_REQUEST;
+    mbox[2] = MBOX_TAG_SETCLKRATE; // set clock rate
+    mbox[3] = 12;
+    mbox[4] = 8;
+    mbox[5] = 2;           // UART clock
+    mbox[6] = 4000000;     // 4Mhz
+    mbox[7] = 0;           // clear turbo
+    mbox[8] = MBOX_TAG_LAST;
+    mbox_call(MBOX_CH_PROP, (void *) mbox);
+
+    // Initialize GPIO pins #14 and #15 as alternative function 0 (UART0).
+    mmio_write(GPIO_FSEL1,
+        (mmio_read(GPIO_FSEL1) & ~((0b111 << 12)|(0b111 << 15)))
+        | (0b100 << 12) | (0b100 << 15));
+
+    // See "GPIO Pull-up/down Clock Registers (GPPUDCLKn)" in "BCM2835 ARM Peripherals"
+    // for more details on this initializaiton sequence.
+    mmio_write(GPIO_PUD, 0);
+    delay(150);
+    mmio_write(GPIO_PUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
+    mmio_write(GPIO_PUDCLK0, 0);
+
+    mmio_write(UART0_CR, 0);            // Disable UART.
+    mmio_write(UART0_ICR, 0x7ff);       // Disable interrupts from UART.
+    mmio_write(UART0_IBRD, 2);          // baud rate = 115200
+    mmio_write(UART0_FBRD, 11);         //
+    mmio_write(UART0_LCRH, 0b11 << 5);  // 8n1
+    mmio_write(UART0_CR, 0x301);        // Enable RX, TX, and UART0.
+}
+
 void arch_printchar(char ch) {
     uart_send(ch);
 }
@@ -24,6 +125,11 @@ char kdebug_readchar(void) {
 bool kdebug_is_readable(void) {
     // TODO: Not yet implemented.
     return false;
+}
+
+void watchdog_init(void) {
+	mmio_write(PM_WDOG, PM_PASSWORD | 10 << 16 /* 10 seconds */);
+	mmio_write(PM_RSTC, PM_PASSWORD | PM_WDOG_FULL_RESET);
 }
 
 void arm64_timer_reload(void) {
@@ -40,6 +146,8 @@ static void timer_init(void) {
 }
 
 void arm64_peripherals_init(void) {
+    uart_init();
+    watchdog_init();
     timer_init();
 }
 
--- kernel/arch/arm64/peripherals.h
+++ kernel/arch/arm64/peripherals.h
@@ -5,12 +5,27 @@
 
 // https://www.raspberrypi.org/documentation/hardware/raspberrypi/bcm2835/BCM2835-ARM-Peripherals.pdf
 // TODO: Let me know if you know the location of the BCM2837 datasheet!
-#define UART0_BASE 0x3f201000
-#define UART0_DR   (UART0_BASE + 0x0000)
-#define UART0_FR   (UART0_BASE + 0x0018)
+
+#define MMIO_BASE2      0x3f000000
+#define UART0_DR        (MMIO_BASE2 + 0x00201000)  // Data register.
+#define UART0_FR        (MMIO_BASE2 + 0x00201018)  // Flag register.
+#define UART0_IBRD      (MMIO_BASE2 + 0x00201024)  // Integer baud rate divisor.
+#define UART0_FBRD      (MMIO_BASE2 + 0x00201028)  // Fractional part of the baud rate divisor.
+#define UART0_LCRH      (MMIO_BASE2 + 0x0020102c)  // Line control registe.
+#define UART0_CR        (MMIO_BASE2 + 0x00201030)  // Control register.
+#define UART0_ICR       (MMIO_BASE2 + 0x00201044)  // Interrupt clear register.
+#define GPIO_FSEL1      (MMIO_BASE2 + 0x00200004)  // GPIO Function Select 1.
+#define GPIO_PUD        (MMIO_BASE2 + 0x00200094)  // GPIO Pin Pull-up/down Enable.
+#define GPIO_PUDCLK0    (MMIO_BASE2 + 0x00200098)  // GPIO Pin Pull-up/down Enable Clock 0.
+#define PM_RSTC         (MMIO_BASE2 + 0x0010001c)
+#define PM_WDOG         (MMIO_BASE2 + 0x00100024)
 
 // https://www.raspberrypi.org/documentation/hardware/raspberrypi/bcm2836/QA7_rev3.4.pdf
 #define CORE0_TIMER_IRQCNTL 0x40000040
+
+// Watchdog timer values.
+#define PM_PASSWORD        0x5a000000
+#define PM_WDOG_FULL_RESET 0x00000020
 
 static inline uint32_t mmio_read(vaddr_t paddr) {
     return *((volatile uint32_t *) from_paddr(paddr));
