160f8ed4a64eb80b1bd8722c9575fe449ae51db1
Seiya Nuta <nuta@seiya.me>
2020-09-09 21:45:29 +0900

    Update

--- libs/resea/task.c
+++ libs/resea/task.c
@@ -23,3 +23,7 @@ error_t task_map(task_t task, vaddr_t vaddr, vaddr_t src, vaddr_t kpage,
                  unsigned flags) {
     return sys_vm_map(task, vaddr, src, kpage, flags);
 }
+
+error_t task_unmap(task_t task, vaddr_t vaddr) {
+    return sys_vm_unmap(task, vaddr);
+}
