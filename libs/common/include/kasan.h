89334208a194351973030ca244ca6acdee65399f
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-12-02 12:02:33 +0530

    Hardcoded shadow size as per size of heap and stack in x64

--- libs/common/include/kasan.h
+++ libs/common/include/kasan.h
@@ -1,10 +1,14 @@
 #include<types.h>
+extern char __heap[];
+extern char __heap_end[];
+extern char __stack[];
+extern char __stack_end[];
 
 void __asan_load8_noabort(vaddr_t addr);
 // Stores the current state of the each memory bytes.
-#define NUM_BYTES 512*1024
+// #define NUM_BYTES (__heap_end - __heap) + (__stack_end - __stack);
+#define NUM_BYTES 0x105000
 uint8_t shadow[NUM_BYTES];  /* .bss size + .data size + heap size */
-
 #define SHADOW_UNADDRESSABLE -1
 // #define SHADOW_NEXT_PTR -1
 // #define SHADOW_CAPACITY -2
