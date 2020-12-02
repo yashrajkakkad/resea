8ed9093cafc23e471d76232245bfaac19633d718
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-12-02 12:28:57 +0530

    Different names for different heap shadow regions and reduedsize of shadow

--- libs/common/include/kasan.h
+++ libs/common/include/kasan.h
@@ -7,12 +7,12 @@ extern char __stack_end[];
 void __asan_load8_noabort(vaddr_t addr);
 // Stores the current state of the each memory bytes.
 // #define NUM_BYTES (__heap_end - __heap) + (__stack_end - __stack);
-#define NUM_BYTES 0x105000
+#define NUM_BYTES 0x10500
 uint8_t shadow[NUM_BYTES];  /* .bss size + .data size + heap size */
 #define SHADOW_UNADDRESSABLE -1
-// #define SHADOW_NEXT_PTR -1
-// #define SHADOW_CAPACITY -2
-// #define SHADOW_SIZE -3
-// #define SHADOW_MAGIC -4
-// #define SHADOW_UNDERFLOW_REDZONE -5
-// #define SHADOW_DATA -6
+#define SHADOW_NEXT_PTR -2
+#define SHADOW_CAPACITY -3
+#define SHADOW_SIZE -4
+#define SHADOW_MAGIC -5
+#define SHADOW_UNDERFLOW_REDZONE -6
+#define SHADOW_DATA -7
